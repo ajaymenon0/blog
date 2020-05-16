fb9b830a229894b4d42de538956f50d615573d56
Ajay Menon <ajaymenon0@gmail.com>
2020-05-16 18:29:45 +0530

    theming

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 const body = document.body;
 
-// body.classList.add("dark");
-
+const settingsBtn = document.getElementById("settings");
+const sidebar = document.getElementById("sidebar");
 const themeSwitcher = document.getElementById("theme");
 
-themeSwitcher.onchange = (e) => {
-  const isChecked = e.target.checked;
-
-  if (isChecked) {
-    body.classList.remove("dark");
-    body.classList.add("light");
-  } else {
-    body.classList.remove("light");
-    body.classList.add("dark");
+if (themeSwitcher) {
+  themeSwitcher.onchange = (e) => {
+    const isChecked = e.target.checked;
+  
+    if (isChecked) {
+      localStorage.setItem("theme", "light");
+      checkAndSetTheme();
+    }
+    else {
+      localStorage.setItem("theme", "dark");
+      checkAndSetTheme();
+    }
   }
 }
+
+const setLightTheme = () => {
+  body.classList.remove("dark");
+  body.classList.add("light");
+}
+
+const setDarkTheme = () => {
+  body.classList.remove("light");
+  body.classList.add("dark");
+}
+
+const checkAndSetTheme = () => {
+  const getTheme = localStorage.getItem("theme") || "light";
+  if (getTheme === "light") setLightTheme();
+  else setDarkTheme();
+}
+
+if (settingsBtn) {
+  settingsBtn.onclick = e => {
+    e.preventDefault();
+    const isHidden = sidebar.classList.contains("hidden");
+    if (isHidden) sidebar.classList.remove("hidden")
+    else sidebar.classList.add("hidden");
+  }
+}
+
+(function () {
+  checkAndSetTheme();
+  const getTheme = localStorage.getItem("theme") || "light";
+  if (themeSwitcher) {
+    if (getTheme === "light") themeSwitcher.checked = true;
+    else themeSwitcher.checked = false;
+  }
+})();
