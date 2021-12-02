84a4ebbb1ddd71d1a02cae703915d98264bcd466
Ajay Menon <ajaymenon0@gmail.com>
2021-12-02 11:28:42 +0530

    Squashed commit of the following:
    commit 11ee5223da69e1984fe9bade203b6403dfef36fc
    Author: Ajay Menon <ajaymenon0@gmail.com>
    Date:   Thu Dec 2 11:17:02 2021 +0530
    
        fix: sorting posts by timestamps
    
    commit 65f7232a584230c35d2071fc41efda34d6f2abe3
    Author: Ajay Menon <ajaymenon0@gmail.com>
    Date:   Wed Dec 1 19:50:45 2021 +0530
    
        feat: adding timestamps
    
    commit 2a0585cf11c185c5102b5f3ce4bed3a7c599182a
    Author: Ajay Menon <ajaymenon0@gmail.com>
    Date:   Wed Dec 1 19:34:41 2021 +0530
    
        build: favicon copy path
    
    commit 147042ee502d492b5777c536d3227f772c2484b5
    Author: Ajay Menon <ajaymenon0@gmail.com>
    Date:   Wed Dec 1 19:21:50 2021 +0530
    
        build: docs for gh-pages
    
    commit ace40a973f534015971c59afcf105d5e22347f71
    Author: Ajay Menon <ajaymenon0@gmail.com>
    Date:   Wed Dec 1 19:14:02 2021 +0530
    
        feat: blog gen for html stuff

---
+++ index.js
@@ -0,0 +1,102 @@
+const { readdirSync, readFileSync } = require('fs');
+const { mkdir, writeFile, readFile } = require('fs/promises');
+const { copy } = require('fs-extra');
+const rimraf = require('rimraf');
+const Handlebars = require('handlebars');
+const cheerio = require('cheerio');
+
+const TEMPLATE_POST = readFileSync('./templates/post.html', 'utf-8');
+const TEMPLATE_MAIN = readFileSync('./templates/main.html', 'utf-8');
+
+const $main = cheerio.load(TEMPLATE_MAIN);
+
+const POSTS_PATH = './posts';
+const BUILD_PATH = './docs';
+const ASSETS_DIRNAME = 'assets';
+const COPYPATHS = ['css/', 'js/', 'favicon.ico'];
+
+
+function build () {
+  const getAllPosts = () =>
+    readdirSync(POSTS_PATH, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+  
+  const allPostDirs = getAllPosts();
+  const allPostMetaData = [];
+  
+  allPostDirs.forEach(dir => {
+    mkdir(`${BUILD_PATH}/${dir}`)
+    .then(() => {
+      console.log(`Created post dir: ${dir}`);
+
+      readFile(`${POSTS_PATH}/${dir}/index.html`)
+      .then(source => {
+        readFile(`${POSTS_PATH}/${dir}/meta.json`, 'utf-8')
+        .then(metadata => {
+          const meta = JSON.parse(metadata);
+          const $ = cheerio.load(TEMPLATE_POST);
+          meta.path = dir;
+          allPostMetaData.push(meta);
+
+          if(allPostMetaData.length === allPostDirs.length) {
+            const sortedPosts = allPostMetaData.sort((a,b) => {
+              const A = new Date(a.postedOn).getTime();
+              const B = new Date(b.postedOn).getTime();
+              return B - A;
+            })
+            sortedPosts.forEach(postmeta => {
+              $main('.posts').append(buildPostCard(postmeta));
+            });
+            writeFile(`${BUILD_PATH}/index.html`, $main.html());
+          }
+
+          copyAssets(dir);
+
+          if(meta.image) {
+            meta.imagepath = `${dir}/assets/${meta.image}`;
+          }
+
+          $('body').append(source.toString());
+          const template = Handlebars.compile($.html());
+          const outputHTML = template(meta);
+          writeFile(`${BUILD_PATH}/${dir}/index.html`, outputHTML);
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    })
+    .catch(() => {
+      console.error('Error in creating post dir');
+    })
+  });
+
+  function copyAssets(path) {
+    copy(`${POSTS_PATH}/${path}/${ASSETS_DIRNAME}`, `${BUILD_PATH}/${path}/${ASSETS_DIRNAME}`)
+    .then()
+    .catch(() => {
+      console.error('🔴');
+    })
+  }
+
+  COPYPATHS.forEach((path) => {
+    copy(`./${path}`, `${BUILD_PATH}/${path}`)
+    .then(() => {
+      console.log(`Copied ${path}`);
+    })
+    .catch(() => {
+      console.error(`Error in copying ${path}`);
+    })
+  });
+}
+
+const buildPostCard = (metadata) => {
+  const dateString = new Date(metadata.postedOn).toDateString();
+  return `<li><a href="./${metadata.path}/">${metadata?.title || ''}</a><br><h6>${dateString}</h6></li>`
+}
+
+rimraf(`${BUILD_PATH}/*`, () => {
+  console.log('BUILD PATH CLEARED!!');
+  build();
+});
